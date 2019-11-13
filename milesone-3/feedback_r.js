df2a8e3dab2b752aca434cc76b59a71bc15f966c
Rabaa-basha <55857464+Rabaa-basha@users.noreply.github.com>
2019-11-12 18:01:51 -0600

    Add files via upload

---
+++ milesone-3/feedback_r.js
@@ -0,0 +1,3 @@
+document.getElementById("feedback_create").addEventListener("keyup", charcountupdate, false)
+document.getElementById("feedback_create").addEventListener("submit", FeedbackForm, false);
+document.getElementById("feedback_create").addEventListener("reset", ReseTForm, false);
