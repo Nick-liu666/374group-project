34ffb2a33b8068d499cf270070e30b1998bfc348
Ncik <liu725@uregina.ca>
2019-11-12 20:23:15 -0600

    change file location

--- milesone-3/feedback_r.js
+++
@@ -1,3 +0,0 @@
-document.getElementById("feedback_create").addEventListener("keyup", charcountupdate, false)
-document.getElementById("feedback_create").addEventListener("submit", FeedbackForm, false);
-document.getElementById("feedback_create").addEventListener("reset", ReseTForm, false);
