9f8f3af29d5bf4a40148ab908108ce7ef8ddef6b
Ncik <liu725@uregina.ca>
2019-12-01 18:26:28 -0600

    update the files

---
+++ milestore-4/Code/contactUs374.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="out374.css">
+	<title>Contact Us</title>
+</head>
+<body>
+	<header>
+	      <img class="logo" src="logo.png" alt="logo" width="10%" height="65%">	<h1 id="header">WEI LAI</h1>
+	</header>
+	<nav>
+	  <li><a href="trial.php" >Homepage</a></li>
+	  <li><a href="elections374.php">Elections</a></li>
+	  <li><a href="AboutUs374.php">About Us</a></li>
+	  <li><a href="feedBack374.php">FeedBack</a></li>
+	  <li><a class = "active" href="contactUs374.php">Contact Us</a></li>
+	</nav>
+
+</br></br></br>
+			<div id="contact">
+				<h3>To contact us </h3>
+				<h3>Email us at:	weilaieleven11@gmail.com</h3>
+				<h3>Phone us at: 	+1 (306)519-3571</h3><br><br><br><br>
+			</div>
+
+			<div id="developers">
+				<h3>Developers?</h3>
+				<h3>Know us better</h3>
+				<h3><a href="https://github.com/Nick-liu666/374group-project.git" id="button"> Github</a></h3>
+			</div>
+	
+			
+
+			
+
+
+
+		<footer>
+		<div id="footer">
+			Copyright &copy; 2019
+		</div>
+	</footer>
+</body>
+</html>
---
+++ milestore-4/README.md
@@ -0,0 +1,5 @@
+# 374group-project
+This project is a web platform to show the candidates' information
+
+## Web link
+* [URL](http://www2.cs.uregina.ca/~liu725/374project/trial.php) - Website
